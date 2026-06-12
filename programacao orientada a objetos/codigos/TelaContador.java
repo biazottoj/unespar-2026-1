114199b4a8a2ebfba1b4cb58451b01cbea7611ff
João Paulo Biazotto <61805911+biazottoj@users.noreply.github.com>
2026-06-12 16:23:33 -0300

    Add files via upload

---
+++ programacao orientada a objetos/codigos/TelaContador.java
@@ -0,0 +1,86 @@
+package view;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.SwingConstants;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class TelaContador extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private Integer contador;
+	private JLabel nCliqueLabel;
+	private JTextField valorInicialField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TelaContador frame = new TelaContador();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public TelaContador() {
+		contador = 0;
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		
+		JButton clicarButton = new JButton("Clique");
+		clicarButton.setVerticalAlignment(SwingConstants.TOP);
+		
+		clicarButton.addActionListener(e -> incrementarContador());
+		
+		contentPane.add(clicarButton);
+		
+		valorInicialField = new JTextField();
+		contentPane.add(valorInicialField);
+		valorInicialField.setColumns(10);
+		
+		nCliqueLabel = new JLabel("Cliques: 0");
+		contentPane.add(nCliqueLabel);
+
+	}
+	
+	private void incrementarContador() {
+		
+		if(valorInicialField.getText().equals("")) {
+			valorInicialField.setText("0");
+		}
+		
+		if(valorInicialField.isEnabled()) {
+			contador = Integer.parseInt(
+					valorInicialField.getText());
+			
+			valorInicialField.setEnabled(false);
+		}
+		
+		contador++;
+		
+		nCliqueLabel.setText("Cliques: " + contador);
+		System.out.println(contador);
+	}
+
+}
